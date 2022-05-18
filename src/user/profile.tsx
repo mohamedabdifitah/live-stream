b6ca4f44d7b3f1290e6588488a23562ae04e7b6f
abdirahmanaali <fxabdirahmanali@gmail.com>
2022-05-18 17:27:45 +0300

    profile ayaan kusoo daray

--- src/user/profile.tsx
+++ src/user/profile.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import AvatarComp from "../scripts/Avatar/Avatar"
 import "../styles/profile.scss"
 const Profile:React.FC = () => {
   return (
      <div className="user-profile">
-      <button> mohamed </button>
+       <div className="content">
+       <AvatarComp isOnline={true} hasStory={true} userName={""} image={"https://random.imagecdn.app/500/150" } callback={"https://random.imagecdn.app/500/150"} className={"user profile"} isLive={false} />
+       </div>
+
+       <div className="descript"></div>
      </div>
     )
 }
