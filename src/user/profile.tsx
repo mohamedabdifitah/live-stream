3e1186bde32ad62d49d9ab78ed3b4e65de4c77db
abdirahmanaali <fxabdirahmanali@gmail.com>
2022-05-20 09:35:08 +0300

    sii wad.

--- src/user/profile.tsx
+++ src/user/profile.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import AvatarComp from "../scripts/Avatar/Avatar"
+import LinkIcon from '@mui/icons-material/Link';
+import {BiLink} from 'react-icons/bi'
 import "../styles/profile.scss"
 const Profile:React.FC = () => {
   return (
@@ -13,12 +15,23 @@ const Profile:React.FC = () => {
               follow
             </button>
            </div>
+           <div className="user-data">
+             <h3>15 Post</h3>
+             <h3>1000 followers</h3>
+             <h3>100 following</h3>
+           </div>
             
        </div>
        </div>
 
+
        <div className="descript">
-         <h3>Ar wuuw.</h3>
+         <h4 className="descript-content">Waxaan nahay walalo is wata.</h4>
+         <div className="descript-link">
+           <BiLink/>
+           <h4 className="link">Description-link.</h4>
+         </div>
+           
        </div>
      </div>
     )
