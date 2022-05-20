8bca62e37b9b65942a7a2e8f41e237f4d1f38b8d
mohamedabdifitah <learntecnology30@gmail.com>
2022-05-20 15:35:58 +0300

    mobile ahan ayaa loo sameyay media queries wax la daho

---
+++ src/Layout/Live.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+const Live = () => {
+  /*React.useEffect(() => { 
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true }) .then((currentStream) => { setStream(currentStream); myVideo.current.srcObject = currentStream; });
+  })
+  */
+  return (
+    <>
+    </>
+    )
+}
+export default Live
--- src/user/profile.tsx
+++ src/user/profile.tsx
@@ -8,17 +8,18 @@ const Profile:React.FC = () => {
      <div className="user-profile">
        <div className="content">
        <AvatarComp isOnline={true} hasStory={true} userName={""} image={"https://random.imagecdn.app/500/150" } callback={"https://random.imagecdn.app/500/150"} className={""} isLive={false} />
-       <div className="bottons">
-           <div className="user-content"> 
-           <h3 className="username-text">Ar wuuw.</h3>
+       <div className="user-content">
+           <div className="user-name"> 
+           <p className="username-text">
+           mohamed abdifitah</p>
            <button className="follow">
               follow
             </button>
            </div>
            <div className="user-data">
              <h3>15 Post</h3>
-             <h3>1000 followers</h3>
-             <h3>100 following</h3>
+             <h3>1000  followers</h3>
+             <h3>100  following</h3>
            </div>
             
        </div>
@@ -27,10 +28,10 @@ const Profile:React.FC = () => {
 
        <div className="descript">
          <h4 className="descript-content">Waxaan nahay walalo is wata.</h4>
-         <div className="descript-link">
-           <BiLink/>
+         <a href="https://google.com" className="descript-link">
+           <BiLink width={"30px"} height={"30px"}/>
            <h4 className="link">Description-link.</h4>
-         </div>
+         </a>
            
        </div>
      </div>
